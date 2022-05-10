b95aadbf02071e9b83945a8d9e346998ff9a4552
Prabhashana Wijesinghe <103393651+Prabha-99@users.noreply.github.com>
2022-05-10 14:52:30 +0530

    just checking

--- src/build/userManagement.java
+++ src/build/userManagement.java
@@ -326,11 +326,11 @@ public class userManagement extends javax.swing.JFrame {
         //</editor-fold>
 
         /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new userManagement().setVisible(true);
-            }
-        });
+//        java.awt.EventQueue.invokeLater(new Runnable() {
+//            public void run() {
+//                new userManagement().setVisible(true);
+//            }
+//        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
